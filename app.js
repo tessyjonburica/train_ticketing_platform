a2d8284eaa6b09233a9993092366aa6ef4dab349
Bharney Adedokun <bharneyadedokun@gmail.com>
2024-02-09 14:56:21 +0100

    updated sql

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const mysql = require('mysql2/promise')
+const app = express()
+const port = 3000
+
+app.use(express.urlencoded({extended: true}));
+
+
+app.get('/', (req, res) => res.send('Hello SwiftRails!'))
+
+app.get('/customers', async (req, res)=>{
+    const connection = await mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'train_ticketing'
+    })
+    let sql = "SELECT * FROM customers"
+    let [results, fields] = await connection.query(sql)
+    res.send(results)
+
+} )
+
+app.post('/customers', async (req, res)=>{
+    let {surname, firstName, email, phone, gender, dateOfBirth, password, nin} = req.body
+    let sql = `INSERT INTO customers (surname, firstName, email, phone, gender, dateOfBirth, password, nin) VALUES ('${surname}', '${firstName}', '${email}', '${phone}', '${gender}', '${password}', '${dateOfBirth}', '${nin}' ) `
+    const connection = await mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'train_ticketing'
+    })
+   
+    const [results, fields] = await connection.query(sql)
+    res.send(results)
+
+} )
+
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- train_ticketing.sql
+++ train_ticketing.sql
@@ -2,24 +2,32 @@ CREATE TABLE IF NOT EXISTS customers(
     `id` INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     `surname` VARCHAR(255) NOT NULL,
     `firstName` VARCHAR(255) NOT NULL,
-    `email`  VARCHAR(255) PRIMARY KEY,
+    `email`  VARCHAR(255),
     `phone` CHAR(20),
     `gender` ENUM('Male','Female') DEFAULT 'Male',
     `dateOfBirth` DATE,
+    `nin` VARCHAR(255),
     `password` VARCHAR(255) NOT NULL,
-    `nin` VARCHAR(25)
     `createdAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `updatedAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-)
+);
 
 CREATE TABLE IF NOT EXISTS schedules(
     `id` INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     `name` VARCHAR(255) NOT NULL,
-    `distance` FLOAT
-    `stationId` INT FOREIGN KEY
+    `distance` FLOAT,
+    `stationId` INT, FOREIGN KEY (`stationId`) REFERENCES stations(id),
     `arrivalTime` TIMESTAMP,
     `departureTime` TIMESTAMP,
-    `departureStation` VARCHAR(255)
+    `departureStation` VARCHAR(255),
     `createdAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `updatedAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-)
+);
+
+CREATE TABLE IF NOT EXISTS seats(
+    `id` INT PRIMARY KEY AUTO_INCREMENT ,
+    `code` VARCHAR(255),
+    `coachId` INT FOREIGN KEY (`coachId`) REFERENCES coaches(id),
+    `classId` INT FOREIGN KEY (`classId`) REFERENCES classes(id),
+    `status` ENUM('Available','Unavailable') DEFAULT 'Available'
+);
