0b9ef2e961de1c2ad5d85d8b09607a89a5edcf46
Bharney Adedokun <bharneyadedokun@gmail.com>
2024-02-15 15:37:27 +0100

    Added express-validator to validate customer data before storing it in the database.

--- app.js
+++ app.js
@@ -11,12 +11,13 @@ const { allSeats, storeSeat, findSeat, updateSeat, deleteSeat } = require('./con
 const { allStations, storeStation, findStation, updateStation, deleteStation } = require('./controllers/stationController');
 const { storeFare, allFares, findFare, updateFare, deleteFare } = require('./controllers/faresController');
 const { allBookedSeats, storeBookedSeat, findBookedSeat, updateBookedSeat, deleteBookedSeat } = require('./controllers/bookedSeatController');
+const customerValidator = require('./validators/customerValidator');
 
 app.use(express.urlencoded({extended: true}));
 
 
 //Customer route
-app.route('/customers').get(allCustomers).post(storeCustomer)
+app.route('/customers').get(allCustomers).post(customerValidator ,storeCustomer)
 app.route('/customers/:id').get(findCustomer).put(updateCustomer).delete(deleteCustomer)
 
 //Schedule route
